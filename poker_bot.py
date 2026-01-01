f9db10e1241b3998d11356ebe8b49a8472f1f491
Laird Spaulding <laird.spaulding@gmail.com>
2026-01-01 16:56:25 -0600

    Refactor poker bot logic: enhance hand strength calculation, implement multi-deck support, and improve betting mechanics

--- poker_bot.py
+++ poker_bot.py
@@ -1,6 +1,6 @@
 import random
 buy_in = 20 # placeholder
-pot = 0
+num_people = 4
 
 class Card:
     def __init__(self, suit, val):
@@ -40,9 +40,12 @@ class Pocket:
         joint_val = card1.val + card2.val
         # Determines if the two cars make a pair
         pair = int(difference == 0)
-        total_strength = joint_val*.5 + suited*20 + pair*50 - difference*5
+        # Determines if a straight is possible given these two cards
+        straight_possible = int(0 < difference <= 4)
+        # Calculate total strength with aces being low
+        total_strength = joint_val*5 + suited*20 + pair*50 - (difference*2 * straight_possible) + straight_possible*20
         # Calculate total strength with aces being high
-        alter_strength = alter_joint_val*.5 + suited*20 + alter_pair*50 - alter_difference*5
+        alter_strength = alter_joint_val*5 + suited*20 + alter_pair*50 - (alter_difference*2 * straight_possible) + straight_possible*20
         # Return whichever strength is stronger
         if total_strength >= alter_strength:
             return total_strength
@@ -60,34 +63,74 @@ class Deck:
         # Represents the four suits of a deck of cards
         self.suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
         self.nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-        self.cards = [Card(suit, val) for suit in self.suits for val in self.nums]
+        # Declare deck as 8 decks of cards, similar to casino style poker
+        self.cards = [Card(suit, val) for i in range(8) for suit in self.suits for val in self.nums]
         self.shuffle()
 
     def shuffle(self):
-        random.shuffle(self.cards)
+        for i in range(20): # Just to be thourough
+            random.shuffle(self.cards)
 
     def deal_card(self):
         if not self.cards:
             self.__init__() # Reshuffle if deck is empty
         return self.cards.pop()
     
-class Round:
+class Round:#
     def __init__(self):
         self.deck = Deck()
         self.hand = Pocket()
         self.middle = Middle()
+        self.current_bet = buy_in # To play a round, you must at least bet the buy in
+        self.pot = buy_in * num_people
+    def raise_bet(self, amount):
+        self.current_bet += amount
+        self.pot += amount
     def deal(self):
         for i in range(2):
             self.hand.add_card(self.deck.deal_card())
-        print(f"Your hand is {self.hand.cards[0].val} of {self.hand.cards[0].suit} and {self.hand.cards[1].val} of {self.hand.cards[1].suit}")
-        print(f"Your hand strength is {str(self.hand.calc_strength())} out of 34")
-    def flop(self):
-        for i in range(3):
+        strength = self.hand.calc_strength()
+        print(f"Hand is {self.hand.cards[0].val} of {self.hand.cards[0].suit} and {self.hand.cards[1].val} of {self.hand.cards[1].suit}")
+        print(f"Hand strength is {str(strength)} out of 210")
+        if strength > 115:
+            self.raise_bet(self.current_bet)
+            print("Raise 2x the buy in")
+        elif strength > 95:
+            self.raise_bet(self.current_bet*.5)
+            print("Raise 1.5x the buy in")
+    class Flop:
+        def __init__(self):
+            self.hand = Pocket()
+            self.deck = Deck()
+            self.middle = Middle()
+            self.round = Round()
+        def deal_flop(self):
+            for i in range(3):
+                self.middle.add_card(self.deck.deal_card())
+            print(f"The flop is a {self.middle.cards[0].val} of {self.middle.cards[0].suit}, a {self.middle.cards[1].val} of {self.middle.cards[1].suit}, and a {self.middle.cards[2].val} of {self.middle.cards[2].suit}")
+            self.round.raise_bet(self.round.current_bet*.5)
+    class Turn:
+        def __init__(self):
+            self.hand = Pocket()
+            self.deck = Deck()
+            self.middle = Middle()
+            self.round = Round()
+        def deal_turn(self):
             self.middle.add_card(self.deck.deal_card())
-    def turn(self):
-        self.middle.add_card(self.deck.deal_card())
-    def river(self):
-        self.middle.add_card(self.deck.deal_card())
+    class River:
+        def __init__(self):
+            self.hand = Pocket()
+            self.deck = Deck()
+            self.middle = Middle()
+            self.round = Round()
+        def deal_river(self):
+            self.middle.add_card(self.deck.deal_card())
 
 round = Round()
+flop = round.Flop()
+turn = round.Turn()
+river = round.River()
 round.deal()
+flop.deal_flop()
+turn.deal_turn()
+river.deal_river()
